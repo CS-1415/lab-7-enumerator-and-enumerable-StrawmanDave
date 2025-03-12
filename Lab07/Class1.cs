b627594ba5ec94690c74d3ae2b2a2e8d01dac984
StrawmanDave <davidgb.strawman@gmail.com>
2025-03-12 09:35:48 -0700

    Lab-06 code without implemented methods but inherted classes and interfaces

---
+++ Lab07/Class1.cs
@@ -0,0 +1,76 @@
+﻿namespace Lab07;
+
+using System.Collections.Generic;
+
+public interface IDoubleEndedCollection<T>
+{
+    T First { get; }
+    T Last { get; }
+    int Length { get; }
+
+    void AddLast(T value);  
+    void AddFirst(T value);
+    void RemoveFirst();     
+    void RemoveLast();                
+    void InsertAfter(DNode<T> dNode, T value);
+    void RemoveByValue(T value);
+    void ReverseList();
+}
+
+public class DNode<T>
+{
+    public T Value {get; set;}
+    public DNode<T>? Previous {get; set;}
+    public DNode<T>? Next {get; set;}
+}
+
+public class DoublyLinkedListNode<T>
+{
+    public T Value {get; set;}
+    public DoublyLinkedListNode<T> Previous {get; set;}
+    public DoublyLinkedListNode<T> Next {get; set;}
+}
+
+public class DoublyLinkedList<T> : LinkedList<T>, IDoubleEndedCollection<T>, IEnumerable<T>
+{
+    private DoublyLinkedListNode<T>? _head = null;
+    private DoublyLinkedListNode<T>? _tail = null;
+    T IDoubleEndedCollection<T>.First => throw new NotImplementedException();
+    T IDoubleEndedCollection<T>.Last => throw new NotImplementedException();
+    public int Length {get;} = 0;
+    
+    public void AddLast(T value)
+    {
+
+    }
+
+    public void AddFirst(T value)
+    {
+        
+    }
+
+    public void RemoveFirst()
+    {
+
+    }
+
+    public void RemoveLast()
+    {
+
+    }
+
+    public void InsertAfter(DNode<T> dNode, T value)
+    {
+
+    }
+
+    public void RemoveByValue(T value)
+    {
+
+    }
+
+    public void ReverseList()
+    {
+
+    }
+}
